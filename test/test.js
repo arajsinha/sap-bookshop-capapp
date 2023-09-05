368a6d6a2bb02510ac911644cb77ce36c4d1efcd
Aryan Raj Sinha <aryan.raj.sinha@sap.com>
2023-09-05 12:00:51 +0000

    TMS Upload

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ const expect = chai.expect;
 
 describe('CAP Application Test', function() {
     it('should pass this basic test', function() {
-        let result = 2 + 2;
-        expect(result).to.equal(4);
+        let result = 3 + 2;
+        expect(result).to.equal(5);
     });
 });
